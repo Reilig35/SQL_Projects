3a66efa8f54f96be2106e28fb2dd908d0c904ed1
William <will2me35@gmail.com>
2021-05-24 19:08:52 +0100

    Final version to send for gradind

--- School_Database_Assignment/School_db.sql
+++ School_Database_Assignment/School_db.sql
@@ -24,19 +24,21 @@ CREATE TABLE Instructors (
 
 --Adding in 2 new columns for foregin keys
 ALTER TABLE Students
-ADD Class_ID INT
+ADD Class INT
 
 ALTER TABLE Students
-ADD Instructor_ID INT
+ADD Instructor INT
 
---Adiing in the foregin keys
+--Adding in the foregin keys
 ALTER TABLE Students
 ADD CONSTRAINT FK_ClassID
-FOREIGN KEY(Class_ID) REFERENCES Classes(Class_ID)
+FOREIGN KEY(Class) REFERENCES Classes(Class_ID) ON UPDATE CASCADE ON DELETE CASCADE
+
 
 ALTER TABLE Students
 ADD CONSTRAINT FK_InstructorID
-FOREIGN KEY(Instructor_ID) REFERENCES Instructors(Instructor_ID)
+FOREIGN KEY(Instructor) REFERENCES Instructors(Instructor_ID) ON UPDATE CASCADE ON DELETE CASCADE
+
 
 --Populating Classes Table
 INSERT INTO Classes (Class_Name)
@@ -66,64 +68,86 @@ INSERT INTO Instructors(Instrustor_Name)
 
 --Updating the Class_ID Column to have a value for each student
 UPDATE Students
-	SET Class_ID = 1
+	SET Class = 1
 	WHERE Student_ID = 1
 ;
 
 UPDATE Students
-	SET Class_ID = 1
+	SET Class = 1
 	WHERE Student_ID = 3
 ;
 
 UPDATE Students
-	SET Class_ID = 1
+	SET Class = 1
 	WHERE Student_ID = 4
 ;
 
 UPDATE Students
-	SET Class_ID = 2
+	SET Class = 2
 	WHERE Student_ID = 2
 ;
 
 UPDATE Students
-	SET Class_ID = 2
+	SET Class = 2
 	WHERE Student_ID = 5
 ;
 
 UPDATE Students
-	SET Class_ID = 2
+	SET Class = 2
 	WHERE Student_ID = 6
 ;
 
 --Updating the Instructor_ID Column to have a value for each student
 UPDATE Students
-	SET Instructor_ID = 1000
+	SET Instructor = 1000
 	WHERE Student_ID = 1
 ;
 
 UPDATE Students
-	SET Instructor_ID = 1000
+	SET Instructor = 1000
 	WHERE Student_ID = 3
 ;
 
 UPDATE Students
-	SET Instructor_ID = 1000
+	SET Instructor = 1000
 	WHERE Student_ID = 4
 ;
 
 UPDATE Students
-	SET Instructor_ID = 1001
+	SET Instructor = 1001
 	WHERE Student_ID = 2
 ;
 
 UPDATE Students
-	SET Instructor_ID = 1001
+	SET Instructor = 1001
 	WHERE Student_ID = 5
 ;
 
 UPDATE Students
-	SET Instructor_ID = 1001
+	SET Instructor = 1001
 	WHERE Student_ID = 6
 ;
 
-SELECT * FROM Students
+--Selecting Instructors Name with an alias
+SELECT Instrustor_Name AS Instructor 
+	FROM Instructors
+;
+
+--Selecting Students Names in alphabetical Order
+SELECT Student_Name AS Pupil
+	FROM Students
+	ORDER BY Student_Name
+;
+
+--Using Inner join to get students, Courses and Instructors
+SELECT Students.Student_Name AS 'Student',
+		Classes.Class_Name AS 'Course',
+		Instructors.Instrustor_Name AS 'Instructor'
+		FROM Students 
+		INNER JOIN Classes on Students.Class = Classes.Class_ID
+		INNER JOIN Instructors on Students.Instructor = Instructors.Instructor_ID
+;
+
+	
+
+	
