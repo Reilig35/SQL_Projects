d6b72333ac7bc7724c1349a0f501e35ee097986c
William <will2me35@gmail.com>
2021-06-04 19:33:53 +0100

    Final version of Libary Database

--- Final_SQL_Assignment.sql
+++ Final_SQL_Assignment.sql
@@ -1,6 +1,13 @@
+/*
+SQL code for Final Project
+Author: William Twomey
+Submitted: 04/06/2021
+*/
+
 --Creating Database
 CREATE DATABASE Libary_Management_Database
 
+--Selecting Database to use
 USE Libary_Management_Database
 
 --Creating Libary Branch Table
@@ -48,16 +55,18 @@ CREATE TABLE PUBLISHER (
 	Publisher_Name VARCHAR (50) PRIMARY KEY NOT NULL,
 	Publisher_Address VARCHAR (50) NOT NULL,
 	Publisher_Phone_No VARCHAR(15) NOT NULL
-);
+)
+;
 
 --Creating Book Loan Table
 CREATE TABLE BOOK_LOANS (
 	Book_ID INT,
 	Branch_ID INT,
 	Card_No INT,
-	Date_OUT VARCHAR(11),
-	Date_DUE VARCHAR(11)
-);
+	Date_OUT DATE,
+	Date_DUE DATE
+)
+;
 
 
 --Adding Foregin Keys to Book Copies Table
@@ -125,7 +134,7 @@ INSERT INTO BORROWER
 	('Jenny Fiend', 'Cork', '258-011-368'),
 	('Scott Hall', 'Mayo', '889-741-166'),
 	('Tom Andrews', 'Dublin', '558-571-014'),
-	('Jucy Hill', 'Cork', '258-011-347')
+	('Lucy Hill', 'Cork', '258-011-347')
 ;
 
 --Populating Table Publisher
@@ -142,8 +151,133 @@ INSERT INTO PUBLISHER
 	('Warrior','Milan', '321-888-741'),
 	('Cheese','Paris', '547-698-111'),
 	('Kat','Cork', '021-874-969')
-	;
+;
 
-SELECT * FROM PUBLISHER
-SELECT * FROM BORROWER
-SELECT * FROM LIBARY_BRANCH
+
+--Populating Books Table
+INSERT INTO BOOKS
+	(Title, Publisher_Name)
+	VALUES
+	('The Lost Tribe','Puffin'),
+	('Fantastic Mr Fox','Ladybird'),
+	('Knight Rider','Eagle'),
+	('Dracula','Paddy Books'),
+	('In Search of Lost Time','Eye See U'),
+	('Ulysses','Paddy Books'),
+	('Don Quixote','Cheese'),
+	('The Great Gatsby','Eagle'),
+	('Moby Dick','Puffin'),
+	('War and Peace','Rockstar'),
+	('Hamlet','Reader'),
+	('Madame Bovary','Cheese'),
+	('Lolita','Warrior'),
+	('Crime and Punishment','Kat'),
+	('The Catcher in the Rye','Eagle'),
+	('The Adventures of Huckleberry Finn','Eagle'),
+	('The Iliad','Paddy Books'),
+	('Catch-22','Eye see U'),
+	('Nineteen Eighty Four','Ladybird'),
+	('To Kill a Mockingbird','Eagle')
+;
+
+--Populating Author Table
+INSERT INTO BOOK_AUTHORS
+	(Book_ID, Author_Name)
+	VALUES
+	(103, 'Bram Stroker'),
+	(101, 'Roald Dhal'),
+	(105, 'Tony Kent'),
+	(110, 'Bill Shakesphere'),
+	(119, 'Harper Lee'),
+	(118, 'George Orwell'),
+	(111, 'Bram Stroker'),
+	(102, 'David Hasselhoff'),
+	(100, 'George Orwell'),
+	(116, 'Bram Stroker')
+;
+
+--Populating Book Copies Table
+INSERT INTO BOOK_COPIES
+	(Book_ID, Branch_ID, Number_of_Copies )
+	VALUES
+	(100, 5, 15),
+	(101, 3, 12),
+	(102, 6, 5),
+	(103, 1, 27),
+	(104, 5, 3),
+	(105, 4, 10),
+	(106, 2, 2),
+	(107, 1, 18),
+	(108, 3, 3),
+	(109, 4, 17),
+	(100, 3, 32)
+;
+
+--Populating Book Loans Table
+INSERT INTO BOOK_LOANS
+	(Book_ID, Branch_ID, Card_No, Date_OUT, Date_DUE)
+	VALUES
+	(100, 3, 16, '2021-04-25', '2021-05-31'),
+	(105, 1, 10, '2021-05-31', '2021-06-10'),
+	(102, 6, 16, '2021-01-10', '2021-03-14'),
+	(101, 3, 12, '2020-12-24', '2021-01-21'),
+	(104, 5, 10, '2021-05-01', '2021-06-28'),
+	(112, 4, 13, '2021-05-31', '2021-06-05'),
+	(109, 2, 10, '2021-05-01', '2021-06-01'),
+	(117, 1, 15, '2021-05-21', '2021-05-31'),
+	(119, 3, 16, '2021-04-17', '2021-05-21'),
+	(116, 1, 14, '2021-05-10', '2021-06-15')
+;
+
+
+--Procedure 1 Find number of copies at Sharpstown of The Lost Tribe
+CREATE PROCEDURE dbo.usp_Copies_Of_LostTribe@Sharpstown
+AS
+SELECT  
+BOOKS.Title,
+LIBARY_BRANCH.BranchName AS 'Branch',
+BOOK_COPIES.Number_of_Copies AS 'Available'
+FROM BOOK_COPIES
+INNER JOIN BOOKS ON BOOK_COPIES.Book_ID = BOOKS.Book_ID
+INNER JOIN LIBARY_BRANCH ON BOOK_COPIES.Branch_ID = LIBARY_BRANCH.Branch_ID
+WHERE Title = 'The Lost Tribe' AND BranchName = 'Sharpstown'
+;
+
+GO
+EXEC dbo.usp_Copies_Of_LostTribe@Sharpstown
+
+
+--Procedure 2 Find number of copies of The Lost Tribe at all branches
+CREATE PROCEDURE dbo.usp_Copies_Of_LostTribe@LibaryBranches
+AS
+SELECT
+BOOKS.Title,
+LIBARY_BRANCH.BranchName AS 'Branch',
+BOOK_COPIES.Number_of_Copies AS 'Available'
+FROM BOOK_COPIES
+INNER JOIN BOOKS ON BOOK_COPIES.Book_ID = BOOKS.Book_ID
+INNER JOIN LIBARY_BRANCH ON BOOK_COPIES.Branch_ID = LIBARY_BRANCH.Branch_ID
+WHERE Title = 'The Lost Tribe'
+;
+
+GO
+EXEC dbo.usp_Copies_Of_LostTribe@LibaryBranches
+
+
+--Procedure 3 Find members without books checked out
+CREATE PROCEDURE dbo.usp_Members_Without_Books
+AS
+SELECT 
+BORROWER.Card_No AS 'Member No',
+BORROWER.Borrower_Name AS 'Name',
+BORROWER.Borrower_Address AS 'City'
+FROM BOOK_LOANS FULL OUTER JOIN BORROWER ON BOOK_LOANS.Card_No = BORROWER.Card_No
+FULL OUTER JOIN BOOKS ON BOOK_LOANS.Book_ID = BOOKS.Book_ID
+WHERE Title IS NULL
+;
+
+GO
+EXEC dbo.usp_Members_Without_Books
+
+
+
